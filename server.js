7d9c397d81fad74094f7f6e81c45521f18a0f6b9
Fred Craft <splatmakle@gmail.com>
2018-01-28 21:17:30 -0500

    Committing Heroku notes

--- server.js
+++ server.js
@@ -66,3 +66,54 @@ app.get("/bad", (req, res) => {
 });
 
 app.listen(3000, () => {console.log("Express is Listening on port 3000")});
+
+
+// https://help.github.com/articles/connecting-to-github-with-ssh/
+
+
+// Heroku node.js example demo url:  https://floating-scrubland-82548.herokuapp.com/ deployed to Heroku
+// Heoku commands:
+// heroku ps  - view number of dyno "engines" that are running your app - scale
+// heroku ps:scale web=0 - scale your app dyno's running on server down to zero e.g. turn it off.
+// heroku logs --tail  - get a look at the stuff logged by your app
+// heroku logs --ps web.1 --tail - view only events logged by the web.1 dyno
+// heroku local        - start a local instance of a heroku hosted app.
+// git push heroku master  -  push changes made locally to heroku git repository.
+// heroku addons:create papertrail  -  add to my heroku deployment the Papertrail logging service
+//                                     Papertrail is an addon to a Heroku deployment
+// heroku addons:open papertrail  - opens a console in Web browser showing log(s) for your app.
+// heroku addons  - provides a list of addons currently associated with a deployment
+// heroku run bash  - start up a console running bash shell on your deployed app's dyno
+// heroku run node  - starut up a console running a node.js instance on your remote app dyno.
+// heroku config:set [environment variable]
+//     heroku config:set TIMES=2  - set the TIMES environment vareiable on a deployed app dyno
+// heroku config  -  display config vars that are set on deployed app dyno
+// heroku releases  -  displayes a listing (audit train) of "versions" of an app deployed on heroku
+    // $ heroku releases
+    // === floating-scrubland-82548 Releases - Current: v5
+    // v5  Set TIMES config va…   splatmakle@gmail.com  2018/01/28 17:38:23 -0500 (~ 18m ago)
+    // v4  Deploy c7b100a9        splatmakle@gmail.com  2018/01/28 16:55:50 -0500 (~ 1h ago)
+    // v3  Deploy 787a0397        splatmakle@gmail.com  2018/01/28 16:00:07 -0500 (~ 1h ago)
+    // v2  Enable Logplex         splatmakle@gmail.com  2018/01/28 15:58:58 -0500 (~ 1h ago)
+    // v1  Initial release        splatmakle@gmail.com  2018/01/28 15:58:58 -0500 (~ 1h ago)
+// heroku releases:rollback v3  - will rollback the running dyno to the previous release.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//asdf
